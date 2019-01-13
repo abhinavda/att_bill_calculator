6d70f79683abc19db109b6a6672f81f0e855c072
abhinav <abhinavpitt@gmail.com>
2019-01-12 18:48:33 -0600

    works now

--- check_att_website.py
+++ check_att_website.py
@@ -5,57 +5,71 @@ import calulating_cost
 
 username=sys.argv[1]
 userpassword=sys.argv[2]
-account_owner=sys.argv[3]
+account_owner=sys.argv[3:] ## Full name should be given. First name followed by last name. Example : Abhinav Damarapati. This was extra charges are remove from main account
 
 ## Intialize
-insurance_overall_cost=32.99
+insurance_overall_cost=34.99
 
 broswer_url="https://www.att.com/my/#/viewBill"
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get(broswer_url)
-time.sleep(10)
-
-## typing the username and password
-browser.find_element_by_xpath('//*[@id="userName"]').send_keys(str(username))
-browser.find_element_by_xpath('//*[@id="password"]').send_keys(str(userpassword))
-browser.find_element_by_xpath('//*[@id="loginButton-lgwgLoginButton"]').click()
+time.sleep(12)
+try:
+    ## typing the username and password
+    browser.find_element_by_xpath('//*[@id="userName"]').send_keys(str(username))
+    browser.find_element_by_xpath('//*[@id="password"]').send_keys(str(userpassword))
+    browser.find_element_by_xpath('//*[@id="loginButton-lgwgLoginButton"]').click()
+except :
+    print "unable to open ATT page"
 
 ## clicks the options, returns false if option is unavailable
 def click_options(xpath_element) :
     try :
-        time.sleep(3)
         browser.find_element_by_xpath(xpath_element).click()
         return True
     except :
         return False
 
-## keys won't be avaiable until page loads fully. This function checks in intervals to see the page is loaded
-def status_check_and_retry(fun_to_call) :
-    count = 1
-    time.sleep(count)
-    click_options('//*[@id="popup-btn-close"]/i') ## Closing popups if they exist
-    click_options('//*[@id="fsrInvite"]/section[3]/button[2]') ## Closing popups if they exist
-    if fun_to_call==False and count < 9 :
-        fun_to_call
-        count+=1
-    else :
-        if count >= 9:
-            print "couldn't find the function"
-        print "found it"
+def close_popups() :
+    try:
+        click_options('//*[@id="popup-btn-close"]/i')  ## Closing popups if they exist
+        click_options('//*[@id="fsrInvite"]/section[3]/button[2]')  ## Closing popups if they exist
+    except :
+        pass
 
 # click billing option
-status_check_and_retry(click_options('//*[@id="myBilling"]/div[2]/a'))
+time.sleep(5)
+status_billing=click_options('//*[@id="myBilling"]/div[2]/a')
+if not status_billing :
+    time.sleep(5)
+    close_popups()
+    click_options('//*[@id="myBilling"]/div[2]/a')
 
 # click wireless_option
-status_check_and_retry(click_options('//*[@id="tab1"]/div[1]/div/i'))
+time.sleep(5)
+status_wireless=click_options('//*[@id="tab1"]/div[1]/div/i')
+if not status_wireless :
+    time.sleep(5)
+    close_popups()
+    click_options('//*[@id="tab1"]/div[1]/div/i')
 
 # Getting all results from page
-res=status_check_and_retry(click_options('//*[@id="printfas1"]'))
-res=res.text
-results_page=res.split('\n') ## gives a list
-
-extra_charge=calulating_cost.internet_charges_per_person(results_page,insurance_overall_cost)
-mobile_charges=calulating_cost.calc_price_each_person_mobile(results_page[4:])
-final_charges_per_user=calulating_cost.calc_amount_owed(mobile_charges,extra_charge,str(account_owner))
-print final_charges_per_user
-print sum(final_charges_per_user.values())
+time.sleep(5)
+try :
+    res=browser.find_element_by_xpath('//*[@id="printfas1"]').text
+    results_page=res.split('\n') ## gives a list
+except :
+    print "failed to get results"
+browser.close()
+##
+if len(results_page) >0 :
+    extra_charge=calculating_cost.internet_charges_per_person(results_page,insurance_overall_cost)
+    mobile_charges=calculating_cost.calc_price_each_person_mobile(results_page[4:])
+    final_charges_per_user=calculating_cost.calc_amount_owed(mobile_charges,extra_charge,str(account_owner).strip().upper(),insurance_overall_cost)
+    #print final_charges_per_user
+    for key, value in final_charges_per_user.iteritems():
+        nam1 = key.split(" ")
+        name = nam1[0][0] + nam1[0][1:].lower() + " " + nam1[1][0] + nam1[1][1:].lower()
+        print "%s : $%.2f"%(name,value)
+    print    
+    print "Total : ",sum(final_charges_per_user.values())
